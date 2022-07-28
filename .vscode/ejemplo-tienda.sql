aeac563d81bcc3e5c598a598f6d93ddc20bac831
Ka1ZenGuy <edgargexpositohdez@gmail.com>
2022-07-28 13:59:20 +0100

    .

---
+++ .vscode/ejemplo-tienda.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS lista_productos;
+
+CREATE TABLE lista_productos (
+        nombre_producto TEXT,
+        precio_producto DECIMAL(6,2)
+);
+
+INSERT INTO lista_productos (
+
+        nombre_producto,
+        precio_producto 
+
+)
+VALUES(
+        'Booster box Cartas magic',
+        150€
+), (
+
+    'Camiseta One Piece',
+     14,95€ 
+), ( 
+    'Booster Box DBS TCG',
+    80€
+
+);
+
+
+
+
+
+
