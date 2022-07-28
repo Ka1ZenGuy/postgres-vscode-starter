64e62571968467337dc9aad7aa0593cfac14767a
Ka1ZenGuy <edgargexpositohdez@gmail.com>
2022-07-28 13:30:17 +0100

    .

---
+++ .vscode/agenda.sql
@@ -0,0 +1,33 @@
+CREATE TABLE agenda (CREATE
+    nombre                         VARCHAR(30),
+    apellidos                       VARCHAR(50),
+    edad                            INTEGER,
+    fecha_de_nacimiento             DATE,
+    profesion                       VARCHAR(90),
+    aficiones                       TEXT,
+    email                           VARCHAR(50),
+    telefono                        VARCHAR(15)
+);
+
+INSERT INTO agenda (
+nombre,
+apellidos,
+edad,
+fecha_de_nacimiento,
+profesion,
+aficiones,
+email,
+telefono
+); 
+VALUES (
+'Alicia',
+'Pérez',
+18,
+'2004-6-14',
+'Estudiante',
+'Futbol, toros, votar'
+'ali333@jaja.com'
+'+34666666666'
+
+
+);
--- .vscode/introduccion-bases-datos.sql
+++ .vscode/introduccion-bases-datos.sql
@@ -1,8 +1,33 @@
 -- Borra la tabla si existe--
 DROP TABLE IF EXISTS lista_dulces;
-
 -- Crea la tabla --
 create table lista_dulces(
     nombre_dulce text,
-    descripcion_dulce text
+    descripcion_dulce text,
+    precio_dulce text,
+    stock_dulce text
 );
+
+-- Insertar datos en la tabla --
+insert into lista_dulces (
+         nombre_dulce,
+        descripcion_dulce,
+        precio_dulce
+    )
+VALUES (
+        'Donut', 
+        'Dulce con agujero',
+        '1,50€'
+    ),(
+        'Pachanga', 
+        'Dulce con crema',
+        '1,25€'
+    ),(
+    'Berlina',
+    'Hojaldre relleno de crema',
+    '2,00€'
+    );
+
+    -- Ver contenido de lista_dulces-- 
+ SELECT * 
+ FROM lista_dulces;
