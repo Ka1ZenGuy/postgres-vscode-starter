4abd76fa369a4d839c11d35ab9370b2ebc6d8a31
Ka1ZenGuy <edgargexpositohdez@gmail.com>
2022-08-08 12:41:35 +0100

    comenzando temas de permisos

--- zoo/consultas zoo.sql
+++ zoo/consultas zoo.sql
@@ -39,92 +39,144 @@
     14.- Queremos saber el porcentaje de animales de sexo masculino.
     15.- Queremos saber el porcentaje de carnívoros en cada hábitat.
 */
-
---@block Borrar todas las tablas de la base de datos si ya existen.
-DROP TABLE IF EXISTS prestamos;
-DROP TABLE IF EXISTS clientes;
-DROP TABLE IF EXISTS cintas;
---@block Crear las tablas necesarias para la base de datos
-CREATE TABLE clientes(
-    numero_de_socio SERIAL PRIMARY KEY NOT NULL,
-    nombre VARCHAR(60) NOT NULL,
-    fecha_de_inscripcion DATE NOT NULL,
-    fecha_ultima_mensualidad DATE NOT NULL
-);
-CREATE TABLE cintas(
-    id_cinta SERIAL PRIMARY KEY NOT NULL,
-    titulo VARCHAR(100) NOT NULL,
-    año_de_lanzamiento DATE,
-    sinopsis TEXT,
-    precio NUMERIC(5,2) NOT NULL
-);
-CREATE TABLE prestamos(
-    id_alquiler SERIAL PRIMARY KEY NOT NULL,
-    fecha_prestamo DATE NOT NULL,
-    fecha_limite_devolucion DATE NOT NULL,
-    pelicula_prestada INTEGER NOT NULL,
-    socio INTEGER NOT NULL,
-    CONSTRAINT cinta_id_cinta_fk
-        FOREIGN KEY (pelicula_prestada)
-        REFERENCES cintas(id_cinta),
-    CONSTRAINT clientes_numero_de_socio_fk
-        FOREIGN KEY (socio)
-        REFERENCES clientes(numero_de_socio)
-);
-
-
--- @block Obtener el precio de cada prestamo
+--@block Queremos poder obtener de forma sencilla cuantas veces ha sido visitado cada animal
 SELECT
-    sum(cintas.precio)
+animales.id_animal, animales.nombre, count(*) AS "Numero de visitas"
 FROM
-    prestamos INNER JOIN cintas
-ON
-    prestamos.pelicula_prestada = cintas.id_cinta;
+visitas_habitats
+INNER JOIN animales
+ON visitas_habitats.id_habitat = animales.id_habitat
+group by animales.id_animal
+order by "Numero de visitas" DESC
+LIMIT 10;
 
--- @block dado el numero de socio, obtener el nombre
+-- @block Queremos poder comprobar si un visitante ha visitado a un animal concreto.
+    SELECT
+    visitantes.id_nombre, animales.id_habitat, count(*) AS "Visitas de Visitante"
+    FROM
+    visitantes
+
+--@block Queremos poder obtener de forma sencilla cuantas veces ha sido visitado cada animal.
 SELECT
-    nombre
-FROM
-    clientes
+    animales.id_animal,
+    animales.nombre,
+    count(*) AS "Numero de visitas"
+FROM 
+    visitas_habitats 
+    INNER JOIN animales 
+    ON visitas_habitats.id_habitat = animales.id_habitat
+GROUP BY animales.id_animal
+ORDER BY "Numero de visitas" DESC;
+
+--@block Queremos poder comprobar si un visitante ha visitado a un animal concreto.
+SELECT 
+    visitantes.id_visitante,
+    animales.id_animal,
+    animales.nombre
+FROM 
+    visitantes 
+    INNER JOIN visitas 
+    ON visitantes.id_visitante = visitas.id_visitante
+    INNER JOIN visitas_habitats
+    ON visitas.id_visita = visitas_habitats.id_visita
+    INNER JOIN animales
+    ON visitas_habitats.id_habitat = animales.id_habitat
 WHERE
-    numero_de_socio = 6;
-
--- @block Nombres de las películas alquiladas por el usuario 6
+    visitantes.id_visitante = 8 AND
+    animales.id_animal = 5;
+--@block Queremos una lista de los 10 hábitats más populares.
 SELECT
-    cintas.titulo
+    habitats.id_habitat, habitats.nombre, count(*) AS "Visitas"
 FROM
-    prestamos INNER JOIN cintas
-ON
-    prestamos.pelicula_prestada = cintas.id_cinta
+    habitats
+    INNER JOIN visitas_habitats
+    ON habitats.id_habitat = visitas_habitats.id_habitat
+GROUP BY habitats.id_habitat
+ORDER BY "Visitas" DESC
+LIMIT 10;
+--@block Queremos saber cual es el animal más popular entre visitantes menores de edad.
+SELECT 
+    animales.id_animal, 
+    animales.nombre, 
+    count(*) AS "Numero de Visitas"
+FROM 
+    visitantes
+    INNER JOIN visitas
+    ON visitantes.id_visitante = visitas.id_visitante
+    INNER JOIN visitas_habitats
+    ON visitas.id_visita = visitas_habitats.id_visita
+    INNER JOIN habitats
+    ON visitas_habitats.id_habitat = habitats.id_habitat
+    INNER JOIN animales
+    ON habitats.id_habitat = animales.id_habitat
+WHERE visitantes.edad < 18
+GROUP BY animales.id_animal
+ORDER BY "Numero de Visitas" DESC
+LIMIT 1;
+--@block Queremos una lista de los animales ancianos (su edad es mayor que su esperanza de vida) para cuidarlos con más mimo.
+SELECT 
+    animales.id_animal, 
+    animales.nombre, 
+    especies.nombre, 
+    animales.edad, 
+    especies.esperanza_de_vida
+FROM animales INNER JOIN especies
+ON animales.id_especie = especies.id_especie
+WHERE animales.edad > especies.esperanza_de_vida;
+--@block Queremos una lista de los animales de especies peligrosas.
+SELECT
+    animales.id_animal, animales.nombre
+FROM animales INNER JOIN especies
+ON animales.id_especie = especies.id_especie
 WHERE
-    prestamos.socio = 6;
+    especies.peligrosidad = 'Alta' OR
+    especies.peligrosidad = 'Media';
+--@block Queremos saber cual es el animal más popular entre personas de género femenino.
+--@block Queremos saber el tamaño total del zoo en metros cuadrados.
+--@block Queremos saber el porcentaje de animales de sexo masculino.
+--@block Queremos saber el porcentaje de carnívoros en cada hábitat.
 
--- @block Mostrar una lista de peliculas y las veces que se ha alquilado
+--@block La distribución de Género de los visitantes que han visitado a animales de peligrosidad baja.
 SELECT
-    cintas.id_cinta, cintas.titulo, count(*) AS "Veces Alquilada"
-FROM
-    prestamos INNER JOIN cintas
-ON
-    prestamos.pelicula_prestada = cintas.id_cinta
-GROUP BY
-    cintas.id_cinta
-ORDER BY "Veces Alquilada" DESC;
+* 
+FROM visitantes
+INNER JOIN visitas
+ON visitantes.id_visitante = visitas.id_visitante
+INNER JOIN visitas_habitats
+on visitas.id_visita = visitas_habitats.id_visita
+INNER JOIN animales
+ON visitas_habitats.id_habitat = animales.id_habitat
+INNER JOIN especies
+on animales.id_especies = especies.id_especie
+WHERE especies.peligrosidad = 'Baja'; 
 
--- @block crear vista veces_alquilada
-CREATE OR REPLACE VIEW veces_alquilada AS
+SELECT especies.id_especie, especies.nombre, string_agg(animales.nombre, ',')
+from animales
+inner join especie
+on animales.id_especia = especies.id_especie
+group by especies.id_especie;
+
+-- @block el peso maximo total de todos los animales que viven en cada habitat
 SELECT
-    cintas.id_cinta, cintas.titulo, count(*) AS "Veces Alquilada"
-FROM
-    prestamos INNER JOIN cintas
-ON
-    prestamos.pelicula_prestada = cintas.id_cinta
-GROUP BY
-    cintas.id_cinta
-ORDER BY "Veces Alquilada" DESC;
+habitats.id_habitat, habitat.nombre, count (animales.id_animal),
+sum (especies.peso_maximo)
+from habitats
+inner join animales
+ON habitats.id_habitat = animales.id_habitat
+inner join especies
+on animales.id_especie = especies.id_especie
+GROUP by habitats.id_habitat;
 
--- @block Consultar vista, filtrando los resultados
-SELECT * FROM veces_alquilada
-WHERE "Veces Alquilada" > 15;
+--@block top 5 de habitats populares por numero de visitas
+select 
+habitats.tipo, count(*) as "visitas"
+from visitas_habitats
+INNER JOIN habitats
+ON visitas_habitats.id_habitat = habitats.id_habitat
+group by habitats.tipo
+order BY "visitas" DESC
+limit 5;
 
---@block
-DROP VIEW veces_alquilada;
+
+
+
